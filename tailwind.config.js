999aca1f483924b0f1873fe735c10816d25132de
JamesUdy <udayamvad@gmail.com>
2023-07-20 19:46:27 +0530

    First egghead.io commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: ['class'],
+  theme: {
+    fontWeight: {
+      thin: '100',
+      hairline: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      bright: '450',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      'extra-bold': '800',
+      black: '900',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
